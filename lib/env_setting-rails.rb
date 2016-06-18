8798652be0b9c29665ed762a449306468aece441
Will Spurgin <will.spurgin@gmail.com>
2016-06-18 16:50:37 -0500

    Don't produce warning if env.rb doesn't exist
    Certain apps may desire placing their configuration in initializers or
    directly in environment files. We'll still use the 'env.rb' file in
    config as a default configuration location, but we don't want to mislead
    developers into thinking that the configuration can __only__ be in
    `config/env.rb`

--- lib/env_setting-rails.rb
+++ lib/env_setting-rails.rb
@@ -8,11 +8,7 @@ class EnvSetting
       end
 
       config.before_configuration do
-        if File.exists?(env_rb_file)
-          load env_rb_file
-        else
-          Rails.logger.warn "Your environment variable configuration could not be found. Please create #{env_rb_file} to set up environment variables at Rails boot."
-        end
+        load env_rb_file if File.exists?(env_rb_file)
       end
     end
   end
